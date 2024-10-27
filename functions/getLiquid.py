9f2aac4c6254a402095e2aa1e5fa5fe0b2bf5953
loic.huart <loic.huart08@gmail.com>
2024-10-27 18:52:14 +0100

    fix: monte plus haut 4

--- functions/getLiquid.py
+++ functions/getLiquid.py
@@ -26,9 +26,9 @@ def getLiquid(step, currentPosition, dispenserEmptyingTime, dispenserFillingTime
         # Else, go right
         rotate("belt", int(slotPosition - currentPosition ), "right")
 
-    rotate("bottle", 525, "up") # monte
+    rotate("bottle", 500, "up") # monte
     sleep(pressed)
-    rotate("bottle", 100, "down")  # descend
+    rotate("bottle", 90, "down")  # descend
     sleep(0.5)
     if(currentPosition < 100): # 100 a la place de 80 pour garder une marge et pas bloquer tout a gauche
         rotate("belt", 80, "right")   # decale droite
@@ -36,7 +36,7 @@ def getLiquid(step, currentPosition, dispenserEmptyingTime, dispenserFillingTime
         rotate("belt", 80, "left")   # decale gauche
 
     sleep(1)
-    rotate("bottle", 425, "down")  # descend
+    rotate("bottle", 410, "down")  # descend
     sleep(max(delayAfter - 1.5, 0)) # 1.5 secondes pour compencer le temps des actions precedentes, si inferieur a 0 sleep(0)
     
     if(currentPosition < 100):
