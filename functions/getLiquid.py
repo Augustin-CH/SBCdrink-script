5feeb873af73dc6d4e6f738034c66cc5f18f805c
loic-huart <loic.huart08@gmail.com>
2024-11-16 11:15:43 +0000

    fix: decale

--- functions/getLiquid.py
+++ functions/getLiquid.py
@@ -34,17 +34,17 @@ def getLiquid(step, currentPosition, dispenserEmptyingTime, dispenserFillingTime
     sleep(pressed)
     rotate("bottle", 90, "down")  # descend
     sleep(0.5)
-    if(currentPosition < 100): # 100 a la place de 80 pour garder une marge et pas bloquer tout a gauche
-        rotate("belt", 70, "right")   # decale droite
-    else:
-        rotate("belt", 70, "left")   # decale gauche
+    # if(currentPosition < 100): # 100 a la place de 80 pour garder une marge et pas bloquer tout a gauche
+    #     rotate("belt", 70, "right")   # decale droite
+    # else:
+    rotate("belt", 70, "left")   # decale gauche
 
     sleep(1)
     initPosition("bottle", 150)
     sleep(max(delayAfter - 1.5, 0)) # 1.5 secondes pour compencer le temps des actions precedentes, si inferieur a 0 sleep(0)
     
-    if(currentPosition < 100):
-        currentPosition = int(slotPosition + 80)
-    else:
-        currentPosition = int(slotPosition - 80)
+    # if(currentPosition < 100):
+    #     currentPosition = int(slotPosition + 70)
+    # else:
+    currentPosition = int(slotPosition - 70)
     return currentPosition
